8448270b45276b0a43704a362d743ccf944d5f35
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2023-01-13 04:36:11 +0000

    added Header

--- app_frontend/components/Header.jsx
+++ app_frontend/components/Header.jsx
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import React, { useState, useEffect } from "react";
+import {
+  BuildingStorefrontIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+  UserGroupIcon,
+  VideoCameraIcon,
+  NewspaperIcon,
+} from "@heroicons/react/24/outline";
+import {
+  HomeIcon as ActiveHomeIcon,
+  BuildingStorefrontIcon as ActiveBuildingStorefrontIcon,
+  VideoCameraIcon as ActiveVideoCameraIcon,
+  UserGroupIcon as ActiveUserGroupIcon,
+  NewspaperIcon as ActiveNewsPaperIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  BellIcon,
+  ArrowLeftOnRectangleIcon,
+} from "@heroicons/react/24/solid";
+import { useStateContext } from "../context/StateContext";
+import Sidebar from './Sidebar';
 
 const Header = () => {
+  const { isMobile } = useStateContext();
+
   return (
     <div className="px-2 flex items-center justify-between bg-white shadow h-[10vh]">
       <LeftHeader />
+      {isMobile ? (
+        <Sidebar />
+      ) : (
+        <>
+          <CenterHeader />
+          <RightHeader />
+        </>
+      )}
     </div>
   );
 };
@@ -14,17 +43,18 @@ const Header = () => {
 const LeftHeader = () => {
   const [hasFocus, setHasFocus] = useState(false);
   const [input, setInput] = useState("");
+  const { isMobile } = useStateContext();
 
   const handleSearch = (e) => {
-      if (e && e.code === "Enter") {
-        const search = input;
+    if (e && e.code === "Enter") {
+      const search = input;
       setHasFocus(false);
       setInput("");
       console.log(search);
     }
   };
   return (
-    <div className="flex items-center gap-2 lg:w-3/12 h-full">
+    <div className={`flex items-center gap-2 ${isMobile ? 'w-4/12': 'w-10/12'} h-full`}>
       <img
         src="https://cdn-icons-png.flaticon.com/128/4494/4494464.png"
         className="w-10 h-10 rounded-full"
@@ -53,5 +83,106 @@ const LeftHeader = () => {
     </div>
   );
 };
+
+const CentreIcons = [
+  {
+    name: "Home",
+    icon: <HomeIcon className="menu-icon text-blue-400 hover:bg-blue-50" />,
+    activeIcon: <ActiveHomeIcon className="menu-icon text-blue-400" />,
+    onClick: () => {},
+  },
+  {
+    name: "Videos",
+    icon: (
+      <VideoCameraIcon className="menu-icon text-violet-400 hover:bg-violet-50" />
+    ),
+    activeIcon: <ActiveVideoCameraIcon className="menu-icon text-violet-400" />,
+    onClick: () => {},
+  },
+  {
+    name: "Shops",
+    icon: (
+      <BuildingStorefrontIcon className="menu-icon text-green-400 hover:bg-green-50" />
+    ),
+    activeIcon: (
+      <ActiveBuildingStorefrontIcon className="menu-icon text-green-400" />
+    ),
+    onClick: () => {},
+  },
+  {
+    name: "Groups",
+    icon: <UserGroupIcon className="menu-icon text-red-400 hover:bg-red-50" />,
+    activeIcon: <ActiveUserGroupIcon className="menu-icon text-red-400" />,
+    onClick: () => {},
+  },
+  {
+    name: "News",
+    icon: (
+      <NewspaperIcon className="menu-icon  text-cyan-400 hover:bg-cyan-50" />
+    ),
+    activeIcon: <ActiveNewsPaperIcon className="menu-icon text-cyan-400" />,
+    onClick: () => {
+      console.log("News");
+    },
+  },
+];
+
+const RightHeaderIcons = [
+  {
+    element: (
+      <div className="flex justify-between gap-2 p-2 cursor-pointer">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/4494/4494464.png"
+          className="w-6 h- 6hover:scale-105"
+        />
+        <span className="font-bold">Gaurav</span>
+      </div>
+    ),
+    onClick: () => {},
+  },
+  {
+    element: <ChatBubbleOvalLeftEllipsisIcon className="menu-icon" />,
+    onClick: () => {},
+  },
+  {
+    element: <BellIcon className="menu-icon" />,
+    onClick: () => {},
+  },
+  {
+    element: <ArrowLeftOnRectangleIcon className="menu-icon" />,
+    onClick: () => {},
+  },
+];
+
+const CenterHeader = () => {
+  const { activeMenu, setActiveMenu } = useStateContext();
+  return (
+    <div className="flex items-center justify-between lg:w-4/12 h-full ">
+      {CentreIcons.map((icon) => (
+        <span
+          onClick={() => {
+            icon.onClick();
+            setActiveMenu(icon.name);
+          }}
+        >
+          {icon.name === activeMenu ? icon.activeIcon : icon.icon}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const RightHeader = () => {
+  return (
+    <div className="flex items-center justify-around ml-10 lg:w-3/12">
+      {RightHeaderIcons.map((element) => (
+        <span className="hover:bg-gray-100 rounded-full">
+          {element.element}
+        </span>
+      ))}
+    </div>
+  );
+};
+
 
 export default Header;
--- app_frontend/context/StateContext.js
+++ app_frontend/context/StateContext.js
@@ -1,16 +1,40 @@
-import {useState, useContext, createContext, useEffect} from 'react';
+import { useState, useContext, createContext, useEffect } from "react";
 
 export const StateContext = createContext();
 
-export const StateProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+export const StateProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("Home");
+  const [width, setWidth] = useState();
 
-    return (
-        <StateContext.Provider value={{user, setUser, loading, setLoading}}>
-            {children}
-        </StateContext.Provider>
-    );
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 1025;
+
+  return (
+    <StateContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        setLoading,
+        activeMenu,
+        setActiveMenu,
+        isMobile,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
