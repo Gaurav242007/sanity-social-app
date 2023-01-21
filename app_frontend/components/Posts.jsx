9898f3c2f853f89189dcdc21487d060e8f9f07bf
Gaurav242007 <gaurav2499kumar+1@gmail.com>
2023-01-21 11:54:48 +0000

    merging

--- app_frontend/components/Posts.jsx
+++ app_frontend/components/Posts.jsx
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import { client } from "../client";
-import { fetchPosts } from "../utils/queries";
+import { fetchPosts, fetchSearchPosts } from "../utils/queries";
 import Spinner from "./Spinner";
+import { useStateContext } from "../context/StateContext";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { searchString } = useStateContext();
 
   useEffect(() => {
     setLoading(true);
-    client
-      .fetch(fetchPosts)
-      .then((data) => {
+    if (searchString) {
+      setLoading(true)
+      const query = fetchSearchPosts(searchString);
+      console.log(query)
+      client.fetch(query).then((data) => {
         setPosts(data);
-        setLoading(false);
-        console.log(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+      });
+      setLoading(false)
+    } else {
+      client
+        .fetch(fetchPosts)
+        .then((data) => {
+          setPosts(data);
+          setLoading(false);
+          console.log(data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [searchString]);
   return (
     <div className="w-full flex flex-col items-center">
       {loading && <Spinner />}
